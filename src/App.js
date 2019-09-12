9c4e22c2e3b6a30a7a8668f13cb5dde3e7b47d5d
Yago Lopes <yagolopeslazaro@gmail.com>
2019-09-12 14:49:23 -0300

    first sections

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div id="container">
+      <header>
+        <div id="title">BUILDER</div>
+        <div id="options">
+          <span>GitHub</span>
+          <span>Docs</span>
+          <span>Pricing</span>
+        </div>
+      </header>
+      <section id="intro">
+        <h1>Drag and drop page building for any site</h1>
+        <span>
+          One simple API integration turns everyone on your team into a web
+          developer
+        </span>
+        <a href="##">TRY IT FREE</a>
+      </section>
+    </div>
+  );
 }
 
 export default App;
