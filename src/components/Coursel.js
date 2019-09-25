31dc319817c40497fde7b1f2ce2b72cceaad8019
Yago Lopes <yagolopeslazaro@gmail.com>
2019-09-25 15:07:18 -0300

    push data for github

--- src/components/Coursel.js
+++ src/components/Coursel.js
@@ -1,8 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemsCarousel from "react-items-carousel";
-import { card, btn } from "./Styles";
+import {
+    card,
+    btn,
+    header,
+    img,
+    strong,
+    small,
+    ul,
+    li,
+    tsmall
+} from "./Styles";
+import api from "../service/api";
 export default () => {
+    useEffect(() => {
+        _push();
+    }, []);
+
+    async function _push() {
+        try {
+            const response = await api.get("/repos");
+
+            setRepositories(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
     const [activeItemIndex, setActiveItemIndex] = useState(0);
+    const [repositories, setRepositories] = useState([]);
+
     const chevronWidth = 40;
     return (
         <ItemsCarousel
@@ -15,10 +41,38 @@ export default () => {
             outsideChevron
             chevronWidth={chevronWidth}
         >
-            <div style={card}>First card</div>
-            <div style={card}>Second card</div>
-            <div style={card}>Third card</div>
-            <div style={card}>Fourth card</div>
+            {repositories.map(repository => (
+                <div style={card} key={repository.id}>
+                    <header style={header}>
+                        <img
+                            style={img}
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <strong style={strong}>{repository.name}</strong>
+                        <small style={small}>{repository.owner.login}</small>
+                    </header>
+
+                    <ul style={ul}>
+                        <li style={li}>
+                            {repository.stargazers_count}
+                            <small style={tsmall}>stars</small>
+                        </li>
+                        <li style={li}>
+                            {repository.forks_count}
+                            <small style={tsmall}>forks</small>
+                        </li>
+                        <li style={li}>
+                            {repository.open_issues_count}
+                            <small style={tsmall}>issues</small>
+                        </li>
+                        <li style={li}>
+                            {repository.lastCommit}
+                            <small style={tsmall}>last comit</small>
+                        </li>
+                    </ul>
+                </div>
+            ))}
         </ItemsCarousel>
     );
 };
--- src/components/Styles.js
+++ src/components/Styles.js
@@ -2,12 +2,45 @@ export const card = {
     marginTop: 20,
     marginLeft: "auto",
     marginRight: "auto",
-    height: 200,
-    width: 200,
     borderRadius: 10,
-    background: "#EEE"
+    background: "#fff",
+    display: "flex",
+    flexDirection: "column"
 };
 
+export const header = {
+    padding: 30,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+};
+export const img = {
+    width: 64
+};
+
+export const strong = {
+    fontSize: 24,
+    marginTop: 10
+};
+
+export const small = {
+    fontSize: 14,
+    color: "#666"
+};
+
+export const ul = {
+    listStyle: "none"
+};
+export const li = {
+    fontWeight: "bold"
+};
+export const tsmall = {
+    fontWeight: "normal",
+    fontSize: 12,
+    color: "#999",
+    fontStyle: "italic"
+};
 export const btn = {
-    background: "#000"
+    background: "#fff",
+    color: "none"
 };
